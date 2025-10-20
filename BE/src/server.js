d42ab3eb1a8085912ec83be936ee24563e6fd644
Nguyen Duy Quang <nguyenduyquang240403@gmail.com>
2025-10-20 10:34:15 +0700

    settup deploy

--- BE/src/server.js
+++ BE/src/server.js
@@ -3,17 +3,32 @@ import taskRouter from './routers/tasksRouters.js'
 import { connectDB } from './config/db.js';
 import dotenv from 'dotenv'
 import cors from 'cors';
+import path from 'path';
 
 dotenv.config();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
+
 
 const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors({origin: 'http://localhost:5173'}));
+
+if(process.env.NODE_ENV !== 'production'){
+    app.use(cors({origin: 'http://localhost:5173'}));
+}
+
 
 app.use("/",taskRouter)
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, "../FE/dist")));
+
+    app.get("*",(request,response)=>{
+        response.sendFile(path.join(__dirname, "../FE/dist/index.html"))
+})
+}
 
 connectDB().then(()=>{
     app.listen(PORT,()=>{
